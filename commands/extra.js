b04e2591a7020bdd99eb6ebd2bd3bff59bdbb980
Excel Amadi <144502732+Xcelsama@users.noreply.github.com>
2024-03-30 17:35:22 +0100

    🎈

--- commands/extra.js
+++ commands/extra.js
@@ -4,9 +4,9 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : BLADE-MD
- * @author : SalmaytOfficial <https://github.com/Bladeh4x>
- * @description : BLADE , A Multi-functional whatsapp bot.
+ * @project_name : BAT-MD
+ * @author : Xcelsama
+ * @description : Secktor,A Multi-functional whatsapp bot.
  * @version 0.0.6
  *
  */
@@ -16,13 +16,13 @@ const PastebinAPI = require("pastebin-js");
 pastebin = new PastebinAPI("EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL");
 cmd({
         pattern: "pastebin",
-        desc: "To check ping",
-        category: "extra",
+        desc: "To check paste",
+        category: "tool🛠",
         filename: __filename,
     },
     async(Void, citel) => {
         if(!citel.quoted) return citel.reply('Please quote any text to get link.')
-        let data = await pastebin.createPaste(citel.quoted.text, "BLADE-Pastebin")
+        let data = await pastebin.createPaste(citel.quoted.text, "BAT-BOT Info-Pastebin")
         citel.reply('_Here is your link._\n'+data)
     }
 );
