4979dd845c6eb593be0ce741de7a7b9470afbb93
Excel Amadi <144502732+Xcelsama@users.noreply.github.com>
2024-03-30 16:20:44 +0100

    🎈

--- config.js
+++ config.js
@@ -3,50 +3,42 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER || '2347045035241'  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
-global.mongodb = process.env.MONGODB_URI || "mongodb+srv://ahil1:787191784abhi@ahil1.kzr1tt3.mongodb.net/?retrywrites=true&w=majority"
-global.port= process.env.PORT || 5000
-global.email = 'maxdavid126@gmail.com'
-global.github = 'https://github.com/Xcelsama/STAR-MD'
-global.location = 'Lahore pakistan'
-global.gurl = 'https://instagram.com/alex_david/' // add your username
-global.sudo = process.env.SUDO || "2347045035241"
-global.devs = '2347045035241'
-global.website = 'https://github.com/Xcelsama/STAR-MD' //wa.me/+91000000000000
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/3f3ae4aace2d012c11509.jpg'
+global.owner = process.env.OWNER_NUMBER.split(",")
+global.mongodb = process.env.MONGODB_URI || "Enter-MongoURI-HERE"
+global.port=5000
+global.email = 'samsamsun789@gmail.com'
+global.github = 'https://github.com/Xcelsama/BAT-MD'
+global.location = 'Lagos,Nigeria'
+global.gurl = 'https://whatsapp.com/channel/0029Va9wmuz8F2pGIURwmo0m' // add your username
+global.sudo =  '2347045035241' || process.env.SUDO  
+global.devs = '2347045035241';
+global.website = 'https://github.com/Xcelsama/BAT-MD' //wa.me/+234000000000
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.ibb.co/TRjdhyd/tumblr-na294unfma1rdzgejo1-500.gif'
 module.exports = {
-  sessionName: process.env.SESSION_ID || "",      //Put Your Session Id Here
-  author:  process.env.PACK_AUTHER ||  'STAR MD BOT',
-  packname:  process.env.PACK_NAME || 'MADE BY EXCEL ',
-  
-  botname:   process.env.BOT_NAME === undefined ? "STAR-MD" : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? 'alexdavid' : process.env.OWNER_NAME,  
-  auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
-  autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
-  antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
-  alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
-  antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '234' : process.env.FAKE_COUNTRY_CODE,
-  readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE,
-  auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER,
-  HANDLERS:  process.env.PREFIX === undefined ? '.' : process.env.PREFIX,
-  warncount : process.env.WARN_COUNT === undefined ? 3 : process.env.WARN_COUNT,
-  disablepm:  process.env.DISABLE_PM === undefined ? false : process.env.DISABLE_PM,
-  levelupmessage:  process.env.LEVEL_UP_MESSAGE === undefined ? false : process.env.LEVEL_UP_MESSAGE,
-  antilink:  process.env.ANTILINK_VALUES === undefined ? 'chat.whatsapp.com' : process.env.ANTILINK_VALUES,
-  antilinkaction: process.env.ANTILINK_ACTION === undefined ? 'remove' : process.env.ANTILINK_ACTION,
-  BRANCH: 'main', 
-  ALIVE_MESSAGE:  process.env.ALIVE_MESSAGE === undefined ? '' : process.env.ALIVE_MESSAGE,
-  autobio:  process.env.AUTO_BIO === undefined ? true : process.env.AUTO_BIO,
-  caption :process.env.CAPTION || "\t*•ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝚂𝚃𝙰𝚁-𝙼𝙳•* ",   //*『sᴜʙsᴄʀɪʙᴇ •  ᴛᴇᴄʜ x』*\n https://whatsapp.com/channel/0029Va9wmuz8F2pGIURwmo0m"),	
-  OPENAI_API_KEY:  process.env.OPENAI_API_KEY === undefined ? false : process.env.OPENAI_API_KEY,
-  heroku:  process.env.heroku === undefined ? false : process.env.heroku,
-  HEROKU: {
-    HEROKU: process.env.HEROKU ||false,
-    API_KEY: process.env.HEROKU_API_KEY === undefined ? '' : process.env.HEROKU_API_KEY,
-    APP_NAME: process.env.HEROKU_APP_NAME === undefined ? '' : process.env.HEROKU_APP_NAME
-},
-  VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.env.VERSION,
-  LANG: process.env.THEME|| 'STAR',
+  botname: process.env.BOT_NAME || '𝙱𝙰𝚃-𝙱𝙾𝚃',
+  ownername: 'Excel' || process.env.OWNER_NAME,
+  sessionName: process.env.SESSION_ID || 'PUT-HERE',
+  author:  process.env.PACK_INFO.split(";")[0] || '', 
+  auto_read_status : process.env.AUTO_READ_STATUS || 'false',
+  packname:   "\t   𓅋E͎x͎c͎e͎l͎𓃮 \n\nᴋɪss ʍᴇ... 💋\n\nᴄʟօsᴇ ყσυɾ ᴇყᴇs... 👁️🙈\n\nʍɪss ʍᴇ...♥️\n\n\n    -ᴘʟᴇᴀsᴇ ᴅᴏɴ'ᴛ ᴛᴀӄᴇ 🚮 \n\n--- ρυвʟɪᴄ вσт нᴇʀᴇ ---" || process.env.PACK_INFO.split(";")[1] ,
+  autoreaction: process.env.AUTO_REACTION || 'off',
+  antibadword : process.env.ANTI_BAD_WORD || 'nobadwordokey',
+  alwaysonline: process.env.ALWAYS_ONLINE || 'false',
+  antifake :  '212' || process.env.FAKE_COUNTRY_CODE ,
+  readmessage: process.env.READ_MESSAGE || 'false',
+  HANDLERS: process.env.PREFIX || [','],
+  warncount : process.env.WARN_COUNT || 1,
+  disablepm: process.env.DISABLE_PM || "true",
+  levelupmessage: process.env.LEVEL_UP_MESSAGE || 'true',
+  antilink: process.env.ANTILINK_VALUES || 'chat.whatsapp.com',
+  antilinkaction: process.env.ANTILINK_ACTION || 'remove',
+  BRANCH: 'main',
+  ALIVE_MESSAGE: process.env.ALIVE_MESSAGE || '•.•:•.•:•:•:•:•:•:•:•☾☼☽•:•.•:•.•:•:•:•:•:•:•\Һɿ αм σηℓιηє нσω ¢αη ι нєℓρ уσυ \n'
+	+'•.•:•.•:•:•:•:•:•:•:•☾☼☽•:•.•:•.•:•:•:•:•:•:•\n\n*ι αм ᴍυℓтι ԃєνιᴄє ωнαтѕαρρ вσт* \n*Cʀєαтєԃ вყ : ૯૪υ︎૯Ն*'
+	+'\n\nWhatsapp : wa.me/2347045034241 \n Subscribe: https://whatsapp.com/channel/0029Va9wmuz8F2pGIURwmo0m'  ,
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY ||"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" ,
+  VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.enqv.VERSION,
+  LANG: process.env.THEME|| 'BAT',
   WORKTYPE: process.env.WORKTYPE === undefined ? 'public' : process.env.WORKTYPE
 };
 
@@ -58,4 +50,3 @@ fs.watchFile(file, () => {
     delete require.cache[file]
 	require(file)
 })
- 
